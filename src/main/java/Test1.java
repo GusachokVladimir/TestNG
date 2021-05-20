88d7dc35c2daa90a323ed96ec7199b1ce0ed3ef7
maryia.bartosh <maryia.bartosh@vizor-games.com>
2021-05-20 22:33:27 +0300

    first commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,79 @@
+import org.testng.annotations.*;
+
+@Test
+public class Test1{
+
+    @Test (groups = {"smokeTest"}, dependsOnMethods = "testSmoke11", priority = 1)
+    public void testSmoke1(){
+        System.out.println("TestSmoke 1");
+    }
+
+    @Test (groups = {"smokeTest"}, dependsOnMethods = "testSmoke12", priority = 1)
+    public void testSmoke11(){
+        System.out.println("TestSmoke 1-1");
+    }
+
+    @Test (groups = {"smokeTest"}, dependsOnMethods = "testSmoke13", priority = 1)
+    public void testSmoke12(){
+        System.out.println("TestSmoke 1-2");
+    }
+
+
+    @Test (groups = {"smokeTest"}, priority = 1)
+    public void testSmoke13(){
+        System.out.println("TestSmoke 1-3");
+    }
+
+    @Test (groups = {"regressTest"}, priority = 2)
+    public void testRegress1(){
+        System.out.println("TestRegress 1");
+    }
+
+    @Test (groups = {"regressTest"}, priority = 2)
+    public void testRegress11(){
+        System.out.println("TestRegress 1-1");
+    }
+
+    @Test (groups = {"regressTest"}, priority = 2)
+    public void testRegress12(){
+        System.out.println("TestRegress 1-2");
+    }
+
+    @Test (groups = {"regressTest"}, priority = 2)
+    public void testRegress13(){
+        System.out.println("TestRegress 1-3");
+    }
+
+    @Test (groups = {"unstableTest"}, priority = 3)
+    public void testUnstable1(){
+        System.out.println("TestUnstable 1");
+    }
+
+    @Test (groups = {"unstableTest"}, priority = 3)
+    public void testUnstable11(){
+        System.out.println("TestUnstable 1-1");
+    }
+
+    @Test (groups = {"unstableTest"}, priority = 3)
+    public void testUnstable12(){
+        System.out.println("TestUnstable 1-2");
+    }
+
+    @Test (groups = {"unstableTest"}, priority = 3)
+    public void testUnstable13(){
+        System.out.println("TestUnstable 1-3");
+    }
+
+    @BeforeClass
+    @Test
+    public void Begin(){
+        System.out.println("Begin");
+    }
+
+    @AfterClass
+    @Test
+    public void Finish(){
+        System.out.println("Finish");
+    }
+
+}
---
+++ src/main/java/Test2.java
@@ -0,0 +1,33 @@
+import org.testng.annotations.*;
+
+@Test
+public class Test2 {
+
+    @DataProvider(name = "db")
+    public static Object[][] dbData (){
+        return new Object[][] {
+                {"parameter1", new Integer(1)},
+                {"parameter2", new Integer(2)}
+        };
+    }
+
+
+    public void testUnstable1(){
+        System.out.println("TestUnstable 1");
+    }
+
+    public void testUnstable11(){
+        System.out.println("TestUnstable 1-1");
+    }
+
+    @Parameters({"parameters"})
+    public void testUnstable12(String parameters){
+        System.out.println("TestUnstable 1-2 " + parameters);
+    }
+
+    @Parameters({"db"})
+    @Test (dataProvider = "db")
+    public void testUnstable13(String e1, int e2) {
+        System.out.println("TestUnstable 1-3 " + e1 + " " + e2);
+    }
+}
